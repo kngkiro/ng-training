085e176bccee138db6125ac4a0448df57994936f
kngkiro <kirolis2002@gmail.com>
2022-08-11 13:52:01 +0200

    2 files modified, 4 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TestingComponent} from './testing/testing.component';
+import {Page2Component} from './page2/page2.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'test', component: TestingComponent },
+  { path: 'page2', component: Page2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
