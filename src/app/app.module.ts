51add7848a63a21d91500560a739babccd45a928
Kevin Latusinski <9928134+kevlatus@users.noreply.github.com>
2022-08-11 12:57:35 +0000

    add components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AbbaComponent } from './abba/abba.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApprenticeComponent } from './apprentice/apprentice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AbbaComponent,
+    ApprenticeComponent,
   ],
   imports: [
-    NgbModule,
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
