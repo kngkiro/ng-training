1b47b3f0bdc49c87d985342df9be18ea72939d4e
majushgl <mshgl@gmx.de>
2022-08-11 14:09:31 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+/*
 import {AbbaComponent} from './abba/abba.component';
-import {ApprenticeComponent} from './apprentice/apprentice.component'
+import {ApprenticeComponent} from './apprentice/apprentice.component';
 
-
+*/
 const routes: Routes = [
-  { path: 'abba', component: AbbaComponent },
-  { path: 'apprentice', component: ApprenticeComponent}
+//  { path: 'abba', component: AbbaComponent },
+//  { path: 'apprentice', component: ApprenticeComponent}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
