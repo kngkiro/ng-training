63be2e136a3bd74693a5634a89e6e8e67b4ff213
LorenzMu <lorenz.mueller.de@gmail.com>
2022-08-12 15:00:14 +0200

    5 files modified

--- src/app/apprentice/apprentice.component.ts
+++ src/app/apprentice/apprentice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { LocalService } from '../local.service';
 
 
@@ -9,9 +10,40 @@ import { LocalService } from '../local.service';
 })
 export class ApprenticeComponent implements OnInit {
 
-  jobs = JSON.parse(this.localStore.getData("jobs"));
+  oldjobs = JSON.parse(this.localStore.getData('jobs'));
 
-  constructor(private localStore: LocalService) { }
+  filtercriteria = this.formBuilder.group({
+    subject: '',
+    startingyear: '',
+  });
+
+  filter(): any {
+    let filteredlist = [];
+    this.oldjobs.forEach((jsonobj) => {
+      let ok = true;
+      if (
+        this.filtercriteria.value.subject != '' &&
+        this.filtercriteria.value.subject != jsonobj.group
+      ) {
+        ok = false;
+      }
+      if (
+        this.filtercriteria.value.startingyear != '' &&
+        this.filtercriteria.value.startingyear != jsonobj.year
+      ) {
+        ok = false;
+      }
+      if (ok) {
+        filteredlist.push(jsonobj);
+      }
+    });
+    console.log(filteredlist);
+    this.jobs = filteredlist;
+    return filteredlist;
+  }
+
+  jobs = this.filter();
+  constructor(private localStore: LocalService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
--- src/app/instructor/instructor.component.ts
+++ src/app/instructor/instructor.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { LocalService } from '../local.service';
-
-
 
 @Component({
   selector: 'app-instructor',
@@ -9,16 +8,46 @@ import { LocalService } from '../local.service';
   styleUrls: ['./instructor.component.css'],
 })
 export class InstructorComponent implements OnInit {
+  oldphases = JSON.parse(this.localStore.getData('jobs'));
 
-  phases = JSON.parse(this.localStore.getData("AI2022"));
+  filtercriteria = this.formBuilder.group({
+    subject: '',
+    startingyear: '',
+  });
 
-  constructor(private localStore: LocalService) { 
+  filter(): any {
+    let filteredlist = [];
+    this.oldphases.forEach((jsonobj) => {
+      let ok = true;
+      if (
+        this.filtercriteria.value.subject != '' &&
+        this.filtercriteria.value.subject != jsonobj.group
+      ) {
+        ok = false;
+      }
+      if (
+        this.filtercriteria.value.startingyear != '' &&
+        this.filtercriteria.value.startingyear != jsonobj.year
+      ) {
+        ok = false;
+      }
+      if (ok) {
+        filteredlist.push(jsonobj);
+      }
+    });
+    console.log(filteredlist);
+    this.phases = filteredlist;
+    return filteredlist;
+  }
+
+  phases = this.filter();
+
+  constructor(
+    private localStore: LocalService,
+    private formBuilder: FormBuilder
+  ) {
     //console.log(JSON.parse(this.localStore.getData("AI2022")));
   }
 
-  
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
