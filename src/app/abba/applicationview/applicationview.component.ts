3bc8f12d8d5b8f54301692c70272dca619083bc9
kngkiro <kirolis2002@gmail.com>
2022-08-12 15:14:45 +0200

    6 files modified, 2 added

---
+++ src/app/abba/applicationview/applicationview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applicationview',
+  templateUrl: './applicationview.component.html',
+  styleUrls: ['./applicationview.component.css']
+})
+export class ApplicationviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
