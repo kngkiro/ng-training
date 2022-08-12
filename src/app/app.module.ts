ebe6656d969c40fcdf5822a28662d04d4a9f4957
majushgl <mshgl@gmx.de>
2022-08-12 11:22:36 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AbbaComponent } from './abba/abba.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsComponent } from './forms/forms.component';
 import { ApprenticeComponent } from './apprentice/apprentice.component';
-import {InstructorComponent} from './instructor/instructor.component';
+import { InstructorComponent } from './instructor/instructor.component';
 
-import {NewphaseComponent} from './instructor/newphase/newphase.component';
-import {ApplicationComponent} from './apprentice/application/application.component';
+import { NewphaseComponent } from './instructor/newphase/newphase.component';
+import { ApplicationComponent } from './apprentice/application/application.component';
 
 
 
